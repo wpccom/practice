273ee138d2e1836ec0528e50cf8d8ca9fab19bff
wpccom <wpccom@naver.com>
2023-01-12 23:30:39 +0900

    230112제대로한것

--- main.js
+++ main.js
@@ -2,53 +2,96 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = new URL('http://localhost:3000' + _url).searchParams;
     var pathname = new URL('http://localhost:3000' + _url).pathname;
 
-    console.log(`url : ` + url);
-    console.log(`queryData : ` + queryData);
-    console.log(`queryData.get('id') : ` + queryData.get('id'));
-    console.log(`pathname : ` + pathname)
+    console.log(`===로그 시작===`);
+    console.log(`queryData : ${queryData}`);
+    console.log(`pathname : ${pathname}`);
+    console.log(`queryData.get('id') : ${queryData.get('id')}`);
 
-    if (pathname == '/index.html') {
-        response.end("index")
-    }
+    if (pathname === '/') {
+        if (queryData.get('id') === null) {
+            console.log('# id = null 내용이 실행됩니다.')
+            fs.readdir('./data', function (err, filelist) {
+                console.log(`filelist 출력 : ${filelist}`);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = `<ul>`;
+                var i = 0;
+                while (i < filelist.length) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i++;
+                }
+                list += '</ul>';
 
-    fs.readdir('./data', function (error, filelist) {
-        console.log(filelist);
+                var template = `
+                    <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  ${list}
+                  <h2>${title}</h2>
+                  <p>${description}</p>
+                </body>
+                </html>
+                    `;
+                response.writeHead(200);
+                response.end(template);
+            });
 
-        var list = `<ol>`;
-        var i = 0;
-        while (i < filelist.length) {
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-        }
+        } else {
+            console.log('# id != null 내용이 실행됩니다.')
+            fs.readdir('./data', function (err, filelist) {
+                console.log(`filelist 출력 : ${filelist}`);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = `<ul>`;
+                var i = 0;
+                while (i < filelist.length) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i++;
+                }
+                list += '</ul>';
 
-        list += `</ol>`
-
-        fs.readFile(`data/${queryData.get('id')}`, 'utf8', function (err, description) {
-            var template = `
-            <!doctype html>
+                fs.readFile(`data/${queryData.get('id')}`, 'utf8', function (err, description) {
+                    var title = queryData.get('id');
+                    var template = `
+                <!doctype html>
             <html>
             <head>
-            <title>WEB1 - ${queryData.get('id')}</title>
-            <meta charset="utf-8">
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
             </head>
             <body>
-            <h1><a href="index.html">WEB</a></h1>
-            ${list}
-            <h2>${queryData.get('id')}</h2>
-            <p>${description}</p>
+              <h1><a href="/">WEB</a></h1>
+                ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
             </body>
             </html>
-    `;
+                `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
 
-            response.writeHead(200);
-            response.end(template);
-        });
-    });
+            });
+        }
+        console.log(`url : ${url}`);
+        console.log(`_url : ${_url}`);
+        console.log(`queryData.id : ${queryData.id}`);
+        console.log(`===로그 종료===`);
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
+
+
 });
 app.listen(3000);
